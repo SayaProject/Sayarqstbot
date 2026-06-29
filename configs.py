f7b9450c56806627f158f7887b00a177b49bbd2e
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-06-29 18:38:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,22 +1,13 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
+    API_ID = int(getenv("30422005", ""))
+    API_HASH = getenv("5170ded206641d73215baf40175a6924", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("-1003894816847", "")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
+    SUDO = list(map(int, getenv("5940554521", "").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
-
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
